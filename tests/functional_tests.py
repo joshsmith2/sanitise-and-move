d768dc68a2901166404f7e31d1f44cc5cf912c28
Josh Smith <joshsmith2@gmail.com>
2014-10-03 18:05:18 +0100

    Before refactoring the way files are handled, to fix 'I/O operation on closed file' error

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -5,47 +5,65 @@ import os
 import inspect
 import shutil
 import subprocess as sp
-import time
+import swisspy
 
 
 class FileTransferTest(unittest.TestCase):
 
     def setUp(self):
         # Variables
-        self.current_running_path = os.path.abspath(inspect.stack()[0][1])
-        self.current_running_dir = os.path.dirname(self.current_running_path)
+        self.current_path = os.path.abspath(inspect.stack()[0][1])
+        self.current_dir = os.path.dirname(self.current_path)
 
         # A dictionary containing all paths which need to be created for the test
         self.source_dir_name = 'test_source/'
         self.dest_dir_name = 'test_dest/'
         self.log_dir_name = 'test_logs/'
 
-        self.test_dirs = {
-            'source': os.path.join(self.current_running_dir, self.source_dir_name),
-            'dest': os.path.join(self.current_running_dir, self.dest_dir_name),
-            'log': os.path.join(self.current_running_dir, self.log_dir_name),
-            }
+        self.source = os.path.join(self.current_dir, self.source_dir_name)
+        self.dest = os.path.join(self.current_dir, self.dest_dir_name)
+        self.log = os.path.join(self.current_dir, self.log_dir_name)
+        self.rootdirs = [self.source, self.log, self.dest]
 
-        self.root_script_dir = os.path.dirname(self.current_running_dir)
+        self.source_subfolders = ['.Hidden', 'To Archive', 'Problem Files', 'Logs']
+        self.log_subfolders = ['syslogs','renamed']
+
+
+
+        self.root_script_dir = os.path.dirname(self.current_dir)
         self.command_path = os.path.abspath(os.path.join(self.root_script_dir,
                                                     'sanitise-and-move.py'
         ))
 
+
         #Construct a list to run the sanitisePaths command using Popen
         self.minimal_command = [self.command_path,
                            '-q',
-                           '-t', self.test_dirs['source'],
-                           '-p', self.test_dirs['dest'],
-                           '-r', os.path.join(self.test_dirs['log'],'renamed'),
-                           '-l', os.path.join(self.test_dirs['log'], 'syslogs')
+                           '-t', self.source,
+                           '-p', self.dest,
+                           '-r', os.path.join(self.log,'renamed'),
+                           '-l', os.path.join(self.log, 'syslogs')
         ]
 
-        self.create_dir_structure()
+        for root_dir in self.rootdirs:
+            self.make_dir_if_not_exists(root_dir)
+        for folder in self.source_subfolders:
+            setattr(self,
+                    folder,
+                    os.path.join(self.source, folder))
+            self.make_dir_if_not_exists(getattr(self, folder))
+
+        for folder in self.log_subfolders:
+            setattr(self,
+                    folder,
+                    os.path.join(self.log, folder))
+            self.make_dir_if_not_exists(getattr(self, folder))
+
 
     def tearDown(self):
-        for dir in self.test_dirs:
+        for dir in self.rootdirs:
             try:
-                shutil.rmtree(self.test_dirs[dir])
+                shutil.rmtree(dir)
             except OSError as e:
                 error_number = e[0]
                 if error_number == 2: #File doesn't exist
@@ -64,27 +82,27 @@ class FileTransferTest(unittest.TestCase):
             else:
                 raise
 
-    def create_dir_structure(self):
-        for d_name in self.test_dirs:
-            self.make_dir_if_not_exists(self.test_dirs[d_name])
-        for d in ['.Hidden', 'To Archive', 'Problem Files', 'Logs']:
-            self.make_dir_if_not_exists(os.path.join(self.test_dirs['source'], d))
-        for d in ['syslogs','renamed']:
-            self.make_dir_if_not_exists(os.path.join(self.test_dirs['log'], d))
+#    def create_dir_structure(self):
+#        for d_name in self.test_dirs:
+#            self.make_dir_if_not_exists(self.test_dirs[d_name])
+#        for d in self.:
+#
+#        for d in ['syslogs','renamed']:
+#            self.make_dir_if_not_exists(os.path.join(self.log, d))
 
     def test_do_not_move_files_not_in_a_directory(self):
-        orphan_file_path = os.path.join(self.test_dirs['source'],'To Archive', 'orphan.txt')
+        orphan_file_path = os.path.join(self.source,'To Archive', 'orphan.txt')
         with open(orphan_file_path, 'w') as orphan_file:
             orphan_file.write("NO MOVEY FILEY")
 
         sp.call(self.minimal_command)
 
-        assert os.path.exists(orphan_file_path)
-        assert not os.path.exists(os.path.join(self.test_dirs['dest'], 'orphan.txt'))
-        assert not os.listdir(os.path.join(self.test_dirs['source'], 'Logs'))
+        self.assertTrue(os.path.exists(orphan_file_path))
+        assert not os.path.exists(os.path.join(self.dest, 'orphan.txt'))
+        assert not os.listdir(os.path.join(self.source, 'Logs'))
 
     def test_clean_files_get_to_dest_safely(self):
-        container_path = os.path.join(self.test_dirs['source'], 'To Archive', 'new_dir')
+        container_path = os.path.join(self.source, 'To Archive', 'new_dir')
         os.mkdir(container_path)
         content_path = os.path.join(container_path, 'file_to_transfer.txt')
         with open(content_path, 'w') as content:
@@ -92,10 +110,37 @@ class FileTransferTest(unittest.TestCase):
 
         sp.call(self.minimal_command)
 
-        assert os.path.exists(os.path.join(self.test_dirs['dest'], 'new_dir'))
+        self.assertTrue(os.path.exists(os.path.join(self.dest, 'new_dir')))
 
 
     # Transfer files without changing the modification time, md5, name etc.
+    def test_file_gets_there_intact(self):
+        full_container = os.path.join(self.source,
+                                      'To Archive',
+                                      'full_container')
+        os.mkdir(full_container)
+        test_file_source = os.path.join(self.current_dir, 'test_file')
+        test_file_dest = os.path.join(self.source,
+                                      'To Archive',
+                                      'full_container',
+                                      'test_file')
+        shutil.copyfile(test_file_source, test_file_dest)
+        correct_md5 = swisspy.get_md5(os.path.join(full_container,
+                                                   'test_file'))
+        correct_mod_time = swisspy.get_mod_time(os.path.join(full_container,
+                                                             'test_file'))
+        os.mkdir(os.path.join(self.source,
+                              'To Archive',
+                              'empty_container'))
+
+        sp.call(self.minimal_command)
+
+        test_file_after = os.path.join(self.dest, 'full_container', 'test_file')
+        observed_md5 = swisspy.get_md5(test_file_after)
+        observed_mod_time = swisspy.get_mod_time(test_file_after)
+
+        self.assertEqual(observed_md5, correct_md5)
+        self.assertEqual(observed_mod_time, correct_mod_time)
 
     # Delete .DS_Store files
 
